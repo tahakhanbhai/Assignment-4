08db893537b803aaef2249a5d6ae71d7e9aaa373
tahakhanbhai <taha.khanbhai@mail.utoronto.ca>
2023-07-11 14:52:57 -0400

    Updated work

--- TAHA_A4.R
+++ TAHA_A4.R
@@ -19,17 +19,45 @@
 #longitude: Longitude coordinate of the sighting
 #Tasks
 
+#NUFORC officials comment on sightings that may be hoax.  Figure out a way (go through the Comments and decide how a proper filter should look like) to identify possible hoax reports. Create a new boolean column "is_hoax", and populate this column with TRUE if the sighting is a possible hoax, FALSE otherwise.
+
 #Write a script that accomplishes the following tasks:
-  
- # Read the data into a data frame (make sure that column names do not have spaces in them).
+library(dplyr)
+# Read the data into a data frame (make sure that column names do not have spaces in them).
+#Used trimws() to ensure the column names do not have any spaces in them
+#Used na.strings to identify cells with empty strings as NA. 
+ufo <- read.csv(trimws('ufo_subset.csv'), na.strings = c("", "NA"))
+
 #Don't forget to visually inspect and compare your data frame to the original csv to make sure that all data is loaded as expected.
+str(ufo)
+summary(ufo)
+
 #Find the rows where Shape information is missing and impute with "unknown".
-#Remove the rows that do not have Country information.
+ufo1 <- ufo %>%
+  mutate(shape = ifelse(is.na(shape), "unknown", shape)) %>%
+  #Remove the rows that do not have Country information.
+  filter(!is.na(country))
 #BONUS: For some of the rows where Country column is blank, the country name is found in the City column in brackets. Where Country info is missing, try to extract the information in brackets in the City column and impute that value in the Country column.
 #Convert Datetime and Date_posted columns into appropriate formats
+ufo1$date_posted <- as.Date(strptime(ufo1$date_posted, format = "%d-%m-%Y"))
+ufo1$datetime <- strptime(ufo1$datetime, format = "%Y-%m-%d %H:%M")
 #NUFORC officials comment on sightings that may be hoax.  Figure out a way (go through the Comments and decide how a proper filter should look like) to identify possible hoax reports. Create a new boolean column "is_hoax", and populate this column with TRUE if the sighting is a possible hoax, FALSE otherwise.
+
+#Create a regex of words that when found in the comment section, likely indicate that the UFO sighting is a hoax. Words are seperated by the or | character, so that all words aread in the grepl function. 
+hoax_comment <- "HOAX| hoax | fake | FAKE | Fake | False | false"
+ufo2 <- ufo1 %>%
+  mutate(is_hoax = grepl(hoax_comment, ufo1$comments))
+  
 #Create a table reporting the percentage of hoax sightings per country.
+hoax_per_country <- ufo2 %>%
+  group_by(country) %>%
+  filter(is_hoax) %>%
+  summarise(percent = n())
+
 #Add another column to the dataset (report_delay) and populate with the time difference in days, between the date of the sighting and the date it was reported.
+ufo3 <- ufo2 %>%
+  mutate(report_delay = date_posted - datetime)
+  
 #Remove the rows where the sighting was reported before it happened.
 #Create a table reporting the average report_delay per country.
 #Check the data quality (missingness, format, range etc) of the "duration seconds" column. Explain what kinds of problems you have identified and how you chose to deal with them, in your comments.
